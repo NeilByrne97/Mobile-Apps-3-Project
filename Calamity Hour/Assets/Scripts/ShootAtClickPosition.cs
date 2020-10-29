0424d494207b5977d44a4db4a933264ec99da158
Neil Byrne <G00343624@gmit.ie>
2020-10-29 16:49:51 +0000

    Can dynamically shoot from any point on screen, fixed rotation of bullets

--- Calamity Hour/Assets/Scripts/ShootAtClickPosition.cs
+++ Calamity Hour/Assets/Scripts/ShootAtClickPosition.cs
@@ -7,7 +7,6 @@ public class ShootAtClickPosition : MonoBehaviour
     public Rigidbody bullet;
     public float force = 10.0f;
     public ForceMode forceMode;
-    public string fireButton = "Fire1";
    
     // Start is called before the first frame update
     void Start()
@@ -18,9 +17,14 @@ public class ShootAtClickPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown(fireButton)) {
-        Rigidbody instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
-        instance.AddForce(transform.forward * force, forceMode);
+        if (Input.GetMouseButton(0)) {  // Left click
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // Where curser is on screen
+        Quaternion rotation = Quaternion.LookRotation(ray.direction);   // Rotation of bullet
+
+        Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)); // Spawn of bullet
+        
+        Rigidbody instance = Instantiate(bullet, transform.position, rotation) as Rigidbody; // Instance of bullet
+        instance.AddForce(ray.direction * force, forceMode); // Add force to bullet
     }
     }
 }
