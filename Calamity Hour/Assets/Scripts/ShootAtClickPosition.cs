29f11b9c9275e8bef593b572467bb4c0b337721f
Neil Byrne <G00343624@gmit.ie>
2020-10-29 16:18:50 +0000

    Added military target and can shoot

---
+++ Calamity Hour/Assets/Scripts/ShootAtClickPosition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAtClickPosition : MonoBehaviour
+{
+    public Rigidbody bullet;
+    public float force = 10.0f;
+    public ForceMode forceMode;
+    public string fireButton = "Fire1";
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown(fireButton)) {
+        Rigidbody instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+        instance.AddForce(transform.forward * force, forceMode);
+    }
+    }
+}
