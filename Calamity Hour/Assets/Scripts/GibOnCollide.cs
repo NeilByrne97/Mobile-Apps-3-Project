c3c8f9f88d0003e232b4839527596f72e87b1829
Neil Byrne <G00343624@gmit.ie>
2020-10-30 14:31:26 +0000

    Added target variants

---
+++ Calamity Hour/Assets/Scripts/GibOnCollide.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GibOnCollide : MonoBehaviour
+{
+    public GameObject gib;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject);
+        Instantiate(gib, transform.position, Quaternion.identity);
+    }
+}
--- Calamity Hour/Assets/Scripts/ShootAtClickPosition.cs
+++ Calamity Hour/Assets/Scripts/ShootAtClickPosition.cs
@@ -17,14 +17,16 @@ public class ShootAtClickPosition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0)) {  // Left click
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // Where curser is on screen
-        Quaternion rotation = Quaternion.LookRotation(ray.direction);   // Rotation of bullet
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    // Where curser is on screen
+            Quaternion rotation = Quaternion.LookRotation(ray.direction);   // Rotation of bullet
 
-        Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)); // Spawn of bullet
-        
-        Rigidbody instance = Instantiate(bullet, transform.position, rotation) as Rigidbody; // Instance of bullet
-        instance.AddForce(ray.direction * force, forceMode); // Add force to bullet
-    }
+            Vector3 spawnPosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)); // Spawn of bullet
+
+            Rigidbody instance = Instantiate(bullet, transform.position, rotation) as Rigidbody; // Instance of bullet
+            instance.AddForce(ray.direction * force, forceMode); // Add force to bullet        }
+
+        }
     }
 }
