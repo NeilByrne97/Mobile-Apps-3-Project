2ae29c83755190e44be718cbeb475e376e028df3
Neil Byrne <G00343624@gmit.ie>
2020-11-17 23:39:14 +0000

    Added player health to UI

---
+++ Calamity Hour/Assets/Scripts/HealthManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager Instance;
+
+    public int startingHealth = 3;
+    public GameObject[] healthIcons;
+
+    private int currentHealth;
+
+    void Start()
+    {
+        Instance = this;
+        currentHealth = startingHealth;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public static void RemoveHealth()
+    {
+        Instance.currentHealth--;
+        Instance.healthIcons[Instance.currentHealth].SetActive(false);
+
+    }
+}
