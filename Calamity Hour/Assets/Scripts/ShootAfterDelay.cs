c33d710608f108e96312895d7afc6100f0c152b8
Neil Byrne <G00343624@gmit.ie>
2020-11-17 21:15:44 +0000

    Added shootOnDelay() and shooting particle system

---
+++ Calamity Hour/Assets/Scripts/ShootAfterDelay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootAfterDelay : MonoBehaviour
+{
+    public float delay = 1.0f;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        Invoke("Shoot", delay);
+    }
+
+    void Update()
+    {
+        animator.SetBool("Shoot", false);
+
+    }
+
+    void Shoot()
+    {
+        // Deal damage to player
+
+        animator.SetBool("Shoot", true);
+    }
+
+}
