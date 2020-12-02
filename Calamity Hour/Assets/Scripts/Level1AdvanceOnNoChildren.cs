4d1178aa85afa8bb1c2a66a9e4bf5d8912851f63
Neil Byrne <G00343624@gmit.ie>
2020-12-02 22:45:29 +0000

    Enemy movement stage 1-7 finished

--- Calamity Hour/Assets/Scripts/Level1AdvanceOnNoChildren.cs
+++ Calamity Hour/Assets/Scripts/Level1AdvanceOnNoChildren.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class Level1AdvanceOnNoChildren : MonoBehaviour
 {
     public NodeMovement NodeMovement;
-    public NodeMovement enemyNodeMovement;
+    public NodeMovement enemyNodeMovement0;
     public NodeMovement enemyNodeMovement1;
+    public NodeMovement enemyNodeMovement2;
+    public NodeMovement enemyNodeMovement3;
+    public NodeMovement enemyNodeMovement4;
     //public Animator targetAnimator;
 
     [SerializeField] int count = 2;
@@ -20,26 +23,58 @@ public class Level1AdvanceOnNoChildren : MonoBehaviour
             if (NodeMovement == null)
             {
                 NodeMovement = GameObject.FindGameObjectWithTag("Player").GetComponent<NodeMovement>();
+
             }
 
-            if (enemyNodeMovement == null)
+            if (enemyNodeMovement0 == null)
             {
-                print("Enemy");
-                enemyNodeMovement = GameObject.FindGameObjectWithTag("Enemy").GetComponent<NodeMovement>();
+                print("Enemy0");
+                // enemyNodeMovement0 = GameObject.FindGameObjectWithTag("Enemy0").GetComponent<NodeMovement>();
+                enemyNodeMovement0.GetComponent<NodeMovement>();
+
+
             }
 
             if (enemyNodeMovement1 == null)
             {
                 print("Enemy1");
-                enemyNodeMovement1 = GameObject.FindGameObjectWithTag("Enemy2").GetComponent<NodeMovement>();
+                enemyNodeMovement1.GetComponent<NodeMovement>();
+                //enemyNodeMovement1.MoveToNextNode();
+
             }
 
+               if (enemyNodeMovement2 == null)
+               {
+                   print("Enemy2");
+                   enemyNodeMovement2.GetComponent<NodeMovement>();
+               }
+
+               if (enemyNodeMovement3 == null)
+               {
+                   print("Enemy3");
+                   enemyNodeMovement3.GetComponent<NodeMovement>();
+               }
+/*
+
+               if (enemyNodeMovement4 == null)
+               {
+                   print("Enemy4");
+                   enemyNodeMovement4.GetComponent<NodeMovement>();
+
+               }
+   */
+
             NodeMovement.MoveToNextNode();
-            enemyNodeMovement.MoveToNextNode();
+            enemyNodeMovement0.MoveToNextNode();
             enemyNodeMovement1.MoveToNextNode();
+            enemyNodeMovement2.MoveToNextNode();
+            enemyNodeMovement3.MoveToNextNode();
+
 
             Destroy(gameObject);
         }
+
+
     }
 
 }
